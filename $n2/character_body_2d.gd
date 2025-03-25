455311a2b082170e78d300183c55f2cffe393ce3
LucasGrala <lucas.grala137@gmail.com>
2025-03-25 17:44:43 -0400

    pre-alpha v 1
    Current Project

---
+++ $n2/character_body_2d.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const GRAVITY = 200.0
+const WALK_SPEED = 200
+
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+		if Input.is_action_pressed("sprint"):
+			velocity.x = -2*WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  WALK_SPEED
+		if Input.is_action_pressed("sprint"):
+			velocity.x = 2*WALK_SPEED
+	else:
+		velocity.x = 0
+
+	# "move_and_slide" already takes delta time into account.
+	move_and_slide()
